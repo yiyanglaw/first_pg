9b833557df7499fb21a7d4b7737c5ee6b755b9d6
lyxc <103878931+yiyanglaw@users.noreply.github.com>
2024-08-16 20:28:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,13 @@ def get_db_connection():
 def create_tables():
     conn = get_db_connection()
     cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )
+    """)
     
     cur.execute("""
     CREATE TABLE IF NOT EXISTS patients (
